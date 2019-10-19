72af14527bf2d1cb8b02db0d29b0f0bb6145c4ea
Pedro Carvalho <pedrosophbc@gmail.com>
2019-10-19 01:31:35 -0300

    Configurando ativação de navbar

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -15,8 +15,8 @@
       <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow">
         <h5 class="my-0 mr-md-auto font-weight-bold">Categorias</h5>
         <nav class="my-2 my-md-0 mr-md-3">
-            <a class="p-2 text-danger" href="{{ route('home') }}">Categorias</a>
-            <a class="p-2 text-dark" href="{{ route('create') }}">Nova Categoria</a>
+            <a class="p-2 {{  request()->routeIs('home')? 'text-danger': 'text-dark' }}" href="{{ route('home') }}">Categorias</a>
+            <a class="p-2 {{ request()->routeIs('create')? 'text-danger': 'text-dark' }}" href="{{ route('create') }}">Nova Categoria</a>
          </nav>
         <a class="btn btn-sm btn-outline-primary" href="#">Sair</a>
      </div>
